ff4218ec39a4dada8601bbb71c796b53c5e0bdd4
hastingsaustin <hastings.austin@student.ccm.edu>
2026-09-10 11:02:24 -0400

    I made it up to the quote

---
+++ character_profile.py
@@ -0,0 +1,22 @@
+
+#Austin Hastings
+#CMP 131 86520
+#Week 3
+#Lab 1
+#Assignment 2
+#10 Sep 2026
+print()
+print("Reggie Dunlop ")
+print("40 years of age")
+print("Hockey coach")
+print()
+print("Interesting fact:" + "Reggie is both Coach and player on the team")
+print("Goal:"+"The team is close to folding and he wishes to save it")
+print("Personality trait:"+"Reggie is known for excessive foul language")
+print()
+print("===========================")
+print("      Character Quote")
+print("===========================")
+print()
+print("I am personally placing a hundred dollar bounty on the head of Tim McCraken.")
+print("He's the head coach and chief punk on that Syracuse team")
